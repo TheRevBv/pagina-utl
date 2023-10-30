ec4d00f4fa8a6e0b52a5053980faa9774904d44f
Joshua Morin <73865022+TheRevBv@users.noreply.github.com>
2023-10-30 00:34:25 -0600

    Agrega cambios generales de img

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,34 +1,38 @@
-import { Header, Wrapper } from "@layout/index";
+import { Wrapper } from "@layout/index";
 import SEO from "@components/seo";
-import HomeAbout from "@components/abouts/home-about";
-import HomeHero from "@components/heros/home-hero";
-import HomeBanner from "@components/banners/home-banner";
-import HomeNewsletter from "@components/newsletter/home-newsletter";
-import Footer from "@layout/footers/footer";
-import HomeClass from "@components/classes/home-class";
+import HomeTwoHero from "@components/heros/home-two-hero";
+import HomeTwoClass from "@components/classes/home-two-class";
 import HomePromotion from "@components/promotions/home-promotion";
+//import promoImgTwo from "@assets/img/promotion/2.png";
+import proImgEgresados from "@assets/img/utl/egresados.jpg";
+import HomeTwoProgram from "@components/programs/home-two-program";
+import HomeAbout from "@components/abouts/home-about";
+import HomeBanner from "@components/banners/home-banner";
+import HomeTwoTeacher from "@components/teachers/home-two-teacher";
+import HomeTwoTestimonial from "@components/testimonials/home-two-testimonial";
 import HomeBlog from "@components/blogs/home-blog";
-import HomeTestimonial from "@components/testimonials/home-testimonial";
-import HomeTeacher from "@components/teachers/home-teacher";
-import HomeProgram from "@components/programs/home-program";
+import HomeTwoGallary from "@components/gallary/home-two-gallary";
+import FooterTwo from "@layout/footers/footerTwo";
+import HeaderTwo from "@layout/headers/headerTwo";
 
-
-export default function Home() {
+export default function HomeTwo() {
   return (
     <Wrapper>
-      <SEO pageTitle={'Home Main'} />
-      <Header />
-      <HomeHero/>
-      <HomeClass/>
-      <HomePromotion/>
-      <HomeProgram/>
-      <HomeAbout/>
-      <HomeBanner/>
-      <HomeTeacher/>
-      <HomeTestimonial/>
-      <HomeBlog/>
-      <HomeNewsletter/>
-      <Footer/>
+      <SEO pageTitle={"Inicio"} />
+      <HeaderTwo />
+      <HomeTwoHero />
+      <HomeTwoClass />
+      <HomePromotion promoImg={proImgEgresados} />
+      {
+        //<HomeTwoProgram />
+      }
+      <HomeAbout />
+      <HomeTwoTeacher />
+      <HomeTwoTestimonial />
+      <HomeBanner />
+      <HomeBlog blog_wrap="bd-blog-content-2" blog_date="bd-blog-date-2" />
+      <HomeTwoGallary />
+      <FooterTwo />
     </Wrapper>
-  )
+  );
 }
